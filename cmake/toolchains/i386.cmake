c52290f559c1f6ae9e750c652359675d27ee3340
Alexis Lopez Zubieta <contact@azubieta.net>
2021-07-01 12:30:32 -0500

    Fix toolchain

--- cmake/toolchains/i386.cmake
+++ cmake/toolchains/i386.cmake
@@ -4,5 +4,18 @@ SET(CMAKE_SYSTEM_NAME Linux)
 SET(CMAKE_SYSTEM_VERSION 1)
 
 # specify the cross compiler
-SET(CMAKE_C_COMPILER   /usr/bin/i686-linux-gnu-gcc)
-SET(CMAKE_CXX_COMPILER /usr/bin/i686-linux-gnu-g++)
+SET(CMAKE_C_COMPILER   /usr/bin/gcc)
+SET(CMAKE_C_COMPILER_ARG1 "-m32")
+SET(CMAKE_CXX_COMPILER /usr/bin/g++)
+SET(CMAKE_CXX_COMPILER_ARG1 "-m32")
+
+# where is the target environment
+SET(CMAKE_FIND_ROOT_PATH /usr/lib/i386-linux-gnu)
+SET(CMAKE_IGNORE_PATH /usr/lib/x86_64-linux-gnu/ /usr/lib/x86_64-linux-gnu/lib/)
+
+# search for programs in the build host directories
+SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+
+# for libraries and headers in the target directories
+SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
